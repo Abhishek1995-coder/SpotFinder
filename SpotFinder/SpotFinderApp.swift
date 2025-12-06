794436c7517a2d6dae042d884e8f437a2d1e552a
Abhishek1995-coder <abhishek.mishra12031995@gmail.com>
2025-12-06 23:47:24 +0530

    Initial Commit

---
+++ SpotFinder/SpotFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotFinderApp.swift
+//  SpotFinder
+//
+//  Created by MAC on 06/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
